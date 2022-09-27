7275280ac66c841f821d1cf49250b50a9e7e3cd7
ali747 <razaafzal748@gmail.com>
2022-09-27 16:18:12 +0500

    Second commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 # STATICFILES_DIRS = (
@@ -145,3 +147,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # STATIC_URL = '/static/'
+#activate django heroku
+django_heroku.settings(locals())
